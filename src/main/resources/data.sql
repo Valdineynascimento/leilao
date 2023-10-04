8ff2959d0d8d32ccb933232959a3d4545d3434ce
Valdineynascimento <valdineyjn@gmail.com>
2023-10-04 20:20:30 -0300

    Atualiza data.sql e Adiciona FinanceiraController.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ insert into usr_usuario (usr_cpf,usr_nome, usr_email, usr_telefone)
     values (22233344455, 'Valdiney', 'valdiney@meu.email', '12999887755');
 insert into end_endereco (end_id, end_estado, end_cidade, end_bairro, end_logradouro, end_numero) 
     values (1, 'SP', 'Sao Jose dos Campos', 'Cidade Morumbi', 'Rua Sete', 70);
+insert into fin_financeira (fin_cnpj,fin_nome, fin_razao_social, fin_email, fin_telefone)
+    values (22233344455000, 'ValdineyInveste', 'Financeira Leal do Brasil SA', 'valdiney@invest.email', '12999887755');
