3895d1556581f45f5b607ccc40760ef76e7e33df
Valdineynascimento <valdineyjn@gmail.com>
2023-10-03 22:23:48 -0300

    Adiciona base do projeto e classe usuario.

---
+++ src/main/java/br/gov/fazenda/receita/leilao/entity/usuario.java
@@ -0,0 +1,57 @@
+package br.gov.fazenda.receita.leilao.entity;
+
+public class usuario {
+    
+    private Long cpf;
+    
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    public usuario() {
+    }
+
+    public usuario(Long cpf, String nome, String email, String telefone) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
