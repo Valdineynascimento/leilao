79d0036d048cb847a398a1beee9687936071f744
Valdineynascimento <valdineyjn@gmail.com>
2023-10-03 23:02:13 -0300

    Adiciona classe UsuarioRepository

---
+++ src/main/java/br/gov/fazenda/receita/leilao/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.gov.fazenda.receita.leilao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository{
+    
+}
