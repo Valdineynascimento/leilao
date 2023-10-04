5b88bd042ba059ef094aed3ed9b43d7af820547f
Valdineynascimento <valdineyjn@gmail.com>
2023-10-04 10:27:23 -0300

    Atualiza squema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+drop table if exists usr_usuario;
+drop table if exists end_endereco;
+
+create table usr_usuario (
+  usr_cpf bigint not null PRIMARY KEY,
+  usr_nome varchar(100) not null,
+  usr_email varchar(100) not null,
+  usr_telefone varchar(100) not null  
+);
+
+create table end_endereco (
+  end_id bigint not null PRIMARY KEY auto_increment,
+  end_estado varchar(50) not null,
+  end_cidade varchar(100) not null,
+  end_bairro varchar(150) not null,
+  end_logradouro varchar(200) not null,
+  end_numero int not null,
+  end_cep int
+);
+
