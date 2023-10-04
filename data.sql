63f83ef7c87397f54828ba9a0b23e6180a285b65
Valdineynascimento <valdineyjn@gmail.com>
2023-10-04 10:28:59 -0300

    Altera pasta dos arquivo data e squema

--- data.sql
+++
@@ -1,2 +0,0 @@
-insert into usr_usuario (usr_nome, usr_email, usr_telefone)
-    values ('Valdiney', 'valdiney@meu.email', '12999887755');
--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists usr_usuario;
-
-create table usr_usuario (
-  usr_cpf bigint not null PRIMARY KEY,
-  usr_nome varchar(100) not null,
-  usr_email varchar(100) not null,
-  usr_telefone varchar(100) not null  
-);
-
