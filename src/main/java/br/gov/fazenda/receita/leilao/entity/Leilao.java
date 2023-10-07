e8a7288c692b928b33876931663ef8766355bd2b
Valdineynascimento <valdineyjn@gmail.com>
2023-10-07 17:03:37 -0300

    Atualiza entity leila com anotações.

--- src/main/java/br/gov/fazenda/receita/leilao/entity/Leilao.java
+++ src/main/java/br/gov/fazenda/receita/leilao/entity/Leilao.java
@@ -1,10 +1,15 @@
 package br.gov.fazenda.receita.leilao.entity;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,15 +22,18 @@ public class Leilao {
     @Column(name = "lei_id")
     private Long id;
 
-    @Column(name = "lei_dataInicial")
+    @Column(name = "lei_data_hora")
     private LocalDateTime dataInicial;
 
-    @Column(name = "lei_dataFinal")
-    private LocalDateTime dataFinal;
-
-    @Column(name = "lei_end_local")
-    private Endereco local;
+    @Column(name = "lei_data_visitacao")
+    private LocalDate dataVisitacao;
 
     @Column(name = "lei_descricao")
     private String descricao;
+    
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "lei_end_id")
+    private Endereco endereco;
+
 }
