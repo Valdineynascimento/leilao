9e746dd0298b5ecbb3b126c657904bcbf5ee51a3
Valdineynascimento <valdineyjn@gmail.com>
2023-10-07 23:31:44 -0300

    Adiciona anotação de relacionamento

--- src/main/java/br/gov/fazenda/receita/leilao/controller/UsuarioController.java
+++ src/main/java/br/gov/fazenda/receita/leilao/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class UsuarioController {
     @GetMapping(value = "/{cpf}")
     public Usuario buscarUsuarioPorId(@PathVariable("cpf") Long cpf) {
         return service.buscarUsuarioPorId(cpf);
+    }
+
+    @PostMapping
+    public Usuario novoUsuario(@RequestBody Usuario usuario) {
+        return service.novoUsuario(usuario);
     }
     
 }
