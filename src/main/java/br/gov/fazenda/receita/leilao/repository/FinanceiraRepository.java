efacb92ea187c17a72b38a19f6abe076d32fed6f
GilbertoSantos-Dev <gilbertosantosdev@gmail.com>
2023-10-08 14:35:12 -0300

    Ficaram somente entidades Item e Lote

--- src/main/java/br/gov/fazenda/receita/leilao/repository/FinanceiraRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.fazenda.receita.leilao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.fazenda.receita.leilao.entity.Financeira;
-
-public interface FinanceiraRepository extends JpaRepository<Financeira, Long>{
-    
-}
--- src/main/java/br/gov/fazenda/receita/leilao/service/FinanceiraService.java
+++
@@ -1,9 +0,0 @@
-package br.gov.fazenda.receita.leilao.service;
-
-import br.gov.fazenda.receita.leilao.entity.Financeira;
-
-public interface FinanceiraService {
-
-    public Financeira buscarFinanceiraPorCnpj(Long cnpj);
-    
-}
