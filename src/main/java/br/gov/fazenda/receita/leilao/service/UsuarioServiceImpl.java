200fa86a6815830d0a30d66c3f5e8e6926f40a7d
Valdineynascimento <valdineyjn@gmail.com>
2023-10-07 23:34:49 -0300

    Adiciona metodo salvar novo usuuario.

--- src/main/java/br/gov/fazenda/receita/leilao/service/UsuarioServiceImpl.java
+++ src/main/java/br/gov/fazenda/receita/leilao/service/UsuarioServiceImpl.java
@@ -1,9 +1,11 @@
 package br.gov.fazenda.receita.leilao.service;
 
+import java.util.HashSet;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.fazenda.receita.leilao.entity.Usuario;
 import br.gov.fazenda.receita.leilao.repository.UsuarioRepository;
@@ -23,10 +25,51 @@ public class UsuarioServiceImpl implements UsuarioService{
         return usuarioOp.get();
     }
 
+    @Transactional
     @Override
     public Usuario novoUsuario(Usuario usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'novoUsuario'");
+        if(usuario == null ||
+                usuario.getNome() == null ||
+                usuario.getNome().isBlank() || 
+                usuario.getEmail() == null ||
+                usuario.getEmail().isBlank() ||
+                usuario.getTelefone() == null ||
+                usuario.getTelefone().isBlank()) {
+            throw new IllegalArgumentException("Usuário com atributos inválidos!");
+        }
+
+        usuario = usuarioRepo.save(usuario);
+
+        return usuario;
     }
+
+    /*@Transactional
+    public Usuario novoUsuario(Usuario usuario) {
+        if(usuario == null ||
+                usuario.getNome() == null ||
+                usuario.getNome().isBlank() || 
+                usuario.getSenha() == null ||
+                usuario.getSenha().isBlank()) {
+            throw new IllegalArgumentException("Usuário com atributos inválidos!");
+        }
+        if(!usuario.getAutorizacoes().isEmpty()) {
+            Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
+            for(Autorizacao autorizacao: usuario.getAutorizacoes()) {
+                Autorizacao autorizacaoBd = buscarAutorizacaoPorId(autorizacao.getId());
+                autorizacoes.add(autorizacaoBd);
+            }
+            usuario.setAutorizacoes(autorizacoes);
+        }
+        Set<Anotacao> anotacoes = usuario.getAnotacoes();
+        usuario.setAnotacoes(new HashSet<Anotacao>());
+        usuario = usuarioRepo.save(usuario);
+        for(Anotacao anotacao: anotacoes) {
+            anotacao.setUsuario(usuario);
+            anotacao = anotacaoRepo.save(anotacao);
+            usuario.getAnotacoes().add(anotacao);
+        }
+        
+        return usuario;
+    }*/
     
 }
