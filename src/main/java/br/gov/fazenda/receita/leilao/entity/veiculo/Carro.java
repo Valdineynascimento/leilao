4689683851b435984ce04048b918561a448f66e6
Valdineynascimento <valdineyjn@gmail.com>
2023-11-01 20:51:29 -0300

    Adiciona classe carro.

---
+++ src/main/java/br/gov/fazenda/receita/leilao/entity/veiculo/Carro.java
@@ -0,0 +1,12 @@
+package br.gov.fazenda.receita.leilao.entity.veiculo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Carro extends Veiculo{
+    
+}
