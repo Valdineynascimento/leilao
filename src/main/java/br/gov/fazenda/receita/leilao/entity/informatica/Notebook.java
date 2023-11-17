e9c879d4b700bb24dac2088f216a3631d05307e3
Valdineynascimento <valdineyjn@gmail.com>
2023-11-17 14:13:10 -0300

    Adiciona classe notebook.

---
+++ src/main/java/br/gov/fazenda/receita/leilao/entity/informatica/Notebook.java
@@ -0,0 +1,23 @@
+package br.gov.fazenda.receita.leilao.entity.informatica;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "not_notebook")
+public class Notebook {
+
+    @Column(name = "not_marca")
+    private String marca;
+
+    @Column(name = "not_memoria_ram")
+    private Integer memoriaRam;
+    
+}
